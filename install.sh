e3535aca0b5ebaefb5df805ea2639fea3e9277c4
Nda-cunh <hydraldev@gmail.com>
2024-09-30 18:36:18 +0200

    Update suprapuzzle installer to 1.2

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cd $(mktemp -d )
-wget https://gitlab.com/-/project/61551072/uploads/d7b8736d90d410b62109fb1a73542efb/suprapuzzle
+wget https://gitlab.com/-/project/61551072/uploads/f72f9ae0d778b44689a9b6f549828c56/suprapuzzle
 chmod +x suprapuzzle
 ./suprapuzzle

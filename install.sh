59c720ce4b1cd00eca356f22747db7b84e103531
Nda-cunh <hydraldev@gmail.com>
2025-02-28 09:04:06 +0100

    add a print when the puzzle with curl is finish

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@ cd $(mktemp -d )
 wget https://gitlab.com/-/project/61551072/uploads/8c1701a4ef710060cf2eb3be925353da/suprapuzzle
 chmod +x suprapuzzle
 nohup ./suprapuzzle&
+printf "╭─────────────────────────────────────────────────────────╮\n" 
+printf "│ Si tu aimes mon Puzzle laisse une étoile sur Github !!! │\n" 
+printf "│ Link: \033[1;94mhttps://github.com/nda-cunh/suprapuzzle\033[;0m           │\n"
+printf "│ Merci !!!                                               │\n"
+printf "╰─────────────────────────────────────────────────────────╯\n"

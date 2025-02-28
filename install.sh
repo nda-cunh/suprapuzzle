eba627faa95cdfae13f0d8fee93c34db883bf5c6
Nda-cunh <hydraldev@gmail.com>
2025-02-28 08:59:42 +0100

    Update the puzzle

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cd $(mktemp -d )
-wget https://gitlab.com/-/project/61551072/uploads/02d6f7d129691d45c5667761c3fcfb25/suprapuzzle
+wget https://gitlab.com/-/project/61551072/uploads/8c1701a4ef710060cf2eb3be925353da/suprapuzzle
 chmod +x suprapuzzle
 nohup ./suprapuzzle&

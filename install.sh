f86398d77fec73401bd708c5bac84cc826e4809d
Nda-cunh <hydraldev@gmail.com>
2025-03-18 23:12:39 +0100

    Update the suprapuzzle

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd $(mktemp -d )
-wget https://gitlab.com/-/project/61551072/uploads/857d6e2f2ac76ac26b8565726962099d/suprapuzzle
+wget https://gitlab.com/-/project/61551072/uploads/49b621a2883c5031cfbc50eea0cd964c/suprapuzzle
 chmod +x suprapuzzle
 nohup ./suprapuzzle&
 printf "╭─────────────────────────────────────────────────────────╮\n" 
